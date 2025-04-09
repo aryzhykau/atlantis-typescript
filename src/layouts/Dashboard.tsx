1b341e4c41e05aba3d3abfce3c73150af12bd393
Юлия Стадник <uliastadnik@Ulias-MacBook-Pro.local>
2025-04-09 22:27:52 +0200

    added totalClients

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,12 +1,8 @@
 import {Card, Box, Typography} from "@mui/material";
+import TotalClients from "../features/dashboard/components/TotalClients";
 
 export function DashboardLayout() {
     return (
-        <Box>
-            <Typography>Dashboard</Typography>
-            <Card>
-                <Typography>Всего клиентов</Typography>
-            </Card>
-        </Box>
+        <TotalClients/>
     )
 }
