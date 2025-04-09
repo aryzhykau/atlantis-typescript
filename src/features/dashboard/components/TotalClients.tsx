f750f522f305a9321732aa907cb648f9f53fa8a1
Юлия Стадник <uliastadnik@Ulias-MacBook-Pro.local>
2025-04-09 22:38:00 +0200

    added styles

--- src/features/dashboard/components/TotalClients.tsx
+++ src/features/dashboard/components/TotalClients.tsx
@@ -1,10 +1,18 @@
 import { Box, Typography } from "@mui/material";
 import { useClients } from "../../clients/hooks/clientManagementHooks"
-import { TipsAndUpdates } from "@mui/icons-material";
+
 
 const TotalClients = ()=> {
     const {clients} = useClients();
-    return <Box>
+    return <Box 
+                display={"flex"} 
+                flexDirection={"column"} 
+                alignItems={"center"} 
+                bgcolor={"paper"} 
+                borderRadius={10}
+                width={"150px"}
+                height={"150px"}
+            >
         <Typography variant={"h4"}>Всего клиентов</Typography>
         <Typography variant={"h2"}>{clients.length}</Typography>
     </Box>
