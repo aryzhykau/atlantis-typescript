514e5b58645ae219f045b6bdb2175837def059a4
Юлия Стадник <uliastadnik@Ulias-MacBook-Pro.local>
2025-04-09 23:04:30 +0200

    added NewClients

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,8 +1,12 @@
 import {Card, Box, Typography} from "@mui/material";
 import TotalClients from "../features/dashboard/components/TotalClients";
+import NewClients from "../features/dashboard/components/NewClients";
 
 export function DashboardLayout() {
     return (
+    <>
         <TotalClients/>
-    )
+        <NewClients></NewClients>
+    </>
+    );
 }
