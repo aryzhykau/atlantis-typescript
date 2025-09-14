b234c7e5149c59ea1561c9e2d83bd9414dac772d
Andrei Ryzhykau <andrei.ryzhykau@outstaff.exness.com>
2025-09-14 11:27:48 +0200

    fixed logout cache issues

--- src/store/apis/userApi.ts
+++ src/store/apis/userApi.ts
@@ -8,6 +8,8 @@ export const userApi = baseApi.injectEndpoints({
                 url: '/users/me',
                 method: 'GET',
             }),
+            // provide a tag so we can invalidate the cached current user when auth changes
+            providesTags: ['User'],
         }),
         getUsers: builder.query<IUser[], void>({
             query: () => ({
